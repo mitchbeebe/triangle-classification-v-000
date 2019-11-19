eef2cbc1acb3cc5f52f259824089701c1d022c46
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-19 02:34:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,4 +3,15 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
   
   def initialize(one, two , three)
+    
+    begin
+    
+    
+  end
+  
+  class TriangleError < StandardError
+    def message
+      puts "stuff stuff stuff"
+    end
+  end
 end
