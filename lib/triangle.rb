e2a930958fed2fda3e7bdbf740bcd84c82a7578f
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-19 03:07:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,23 +3,25 @@ class Triangle
   attr_accessor :sides
   
   def initialize(one, two, three)
-    @sides = [one, two, three].sort
+    @sides = [one, two, three]
   end
   
   def kind
     tri_ineq = self.sides[0] + self.sides[1] > self.sides[2] && self.sides[0] + self.sides[2] > self.sides[1] && self.sides[1] + self.sides[2] > self.sides[0]
     if self.sides.all? { |x| x > 0 } && tri_ineq
+      if self.sides.uniq.size == 1
+        :equilateral
+      elsif self.sides.uniq.size == 2
+        :isosceles
+      else
+        :scalene
+      end
+    else
       begin
         raise TriangleError
       rescue TriangleError => error
         puts error.message
       end
-    elsif self.sides.uniq.size == 1
-      :equilateral
-    elsif self.sides.uniq.size == 2
-      :isosceles
-    else
-      :scalene
     end
   end
   
